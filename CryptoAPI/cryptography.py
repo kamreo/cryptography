c55b9bdb5fdefc491111937874be60586d2dff90
Kamil <kamil.jonak@linkfactory.dk>
2022-06-11 14:04:06 +0200

    blockchain project set up

--- CryptoAPI/cryptography.py
+++ CryptoAPI/cryptography.py
@@ -1,4 +1,4 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet  
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
